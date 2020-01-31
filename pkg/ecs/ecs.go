9f4d227e8c311c249c0be9f1de9d956f96e72e73
Ray <ray@raybarrera.com>
2020-01-30 21:24:27 -0800

    added comments to make the linter shut up

--- pkg/ecs/ecs.go
+++ pkg/ecs/ecs.go
@@ -1,21 +1,26 @@
 package ecs
 
+// Entity is a collection of components
 type Entity struct {
 	id         uint64
 	components []Component
 }
 
+// Component is a data container
 type Component struct {
 }
 
+// Ticker does nothing currently
 type Ticker interface {
 	onUpdate(dt float32)
 }
 
+// Start does nothing currently
 func (*Entity) Start() {
 
 }
 
+// AddComponent adds a component to an entity
 func (e *Entity) AddComponent(c Component) error {
 	e.components = append(e.components, c)
 	return nil
