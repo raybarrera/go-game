42fbc114e76f5988300f0cc841beb1b153f86c79
Ray Barrera <ray@raybarrera.com>
2022-03-14 21:40:33 -0700

    modified window size

--- internal/game/game.go
+++ internal/game/game.go
@@ -53,7 +53,7 @@ func (g *Game) Draw(screen *ebiten.Image) {
 // Layout takes the outside size (e.g., the window size) and returns the (logical) screen size.
 // If you don't have to adjust the screen size with the outside size, just return a fixed size.
 func (g *Game) Layout(outsideWidth, outsideHeight int) (screenWidth, screenHeight int) {
-	return 320, 240
+	return 640, 480
 }
 
 func (g *Game) toggleDebugConsole(key ebiten.Key) {
