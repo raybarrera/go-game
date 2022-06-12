59192855248846c83024250eceea92093ff2d20c
Ray Barrera <ray@raybarrera.com>
2022-06-11 21:46:25 -0700

    added a bunch of types defined in by Tiled

--- pkg/tiled/tiled.go
+++ pkg/tiled/tiled.go
@@ -56,5 +56,105 @@ func ParseMapString(mapString string) (TileMap, error) {
 }
 
 type Layer struct{}
-type Property struct{}
-type TileSet struct{}
+type Property struct {
+	Name         string      `json:"name"`
+	Type         string      `json:"type"`
+	PropertyType string      `json:"propertytype"`
+	Value        interface{} `json:"value"`
+}
+
+type TileSet struct {
+	BackgroundColor  string          `json:"backgroundcolor"`
+	Columns          int             `json:"columns"`
+	FirstGID         int             `json:"firstgid"`
+	Grid             Grid            `json:"grid"`
+	Image            string          `json:"image"`
+	ImageHeight      int             `json:"imageheight"`
+	ImageWidth       int             `json:"imagewidth"`
+	Margin           int             `json:"margin"`
+	Name             string          `json:"name"`
+	ObjectAlignment  string          `json:"objectalignment"`
+	Properties       []Property      `json:"properties"`
+	Source           string          `json:"source"`
+	Spacing          int             `json:"spacing"`
+	Terrains         []Terrain       `json:"terrains"`
+	TileCount        int             `json:"tilecount"`
+	TiledVersion     string          `json:"tiledversion"`
+	TileHeight       int             `json:"tileheight"`
+	TileOffset       TileOffset      `json:"tileoffset"`
+	Tiles            []Tile          `json:"tiles"`
+	TileWidth        int             `json:"tilewidth"`
+	Transformations  Transformations `json:"transformations"`
+	TransparentColor string          `json:"transparentcolor"`
+	Type             string          `json:"type"`
+	Version          string          `json:"version"`
+}
+
+type Tile struct {
+	Animation   []Frame    `json:"animation"`
+	Id          int        `json:"id"`
+	Image       string     `json:"image"`
+	ImageHeight int        `json:"imageheight"`
+	ImageWidth  int        `json:"imagewidth"`
+	ObjectGroup Layer      `json:"objectgroup"`
+	Probability float32    `json:"probability"`
+	Properties  []Property `json:"properties"`
+	Terrain     []int      `json:"terrain"`
+	Type        string     `json:"type"`
+}
+
+type Frame struct {
+	Duration int `json:"duration"`
+	TileId   int `json:"tileid"`
+}
+
+type Terrain struct {
+	Name       string     `json:"name"`
+	Properties []Property `json:"properties"`
+	Tile       int        `json:"tile"`
+}
+
+type Grid struct {
+	Height      int    `json:"height"`
+	Width       int    `json:"width"`
+	Orientation string `json:"orientation"`
+}
+
+type TileOffset struct {
+	X int `json:"x"`
+	Y int `json:"y"`
+}
+
+type Transformations struct {
+	HFlip               bool `json:"hflip"`
+	VFlip               bool `json:"vflip"`
+	Rotate              bool `json:"rotate"`
+	PreferUntransformed bool `json:"preferuntransformed"`
+}
+
+type Object struct {
+	Ellipse    bool       `json:"ellipse"`
+	GId        int        `json:"gid"`
+	Height     int        `json:"height"`
+	Width      int        `json:"width"`
+	Id         int        `json:"id"`
+	Name       string     `json:"name"`
+	Point      bool       `json:"point"`
+	Polygon    []Point    `json:"polygon"`
+	Polyline   []Point    `json:"polyline"`
+	Properties []Property `json:"properties"`
+	Rotation   float64    `json:"rotation"`
+	Template   string     `json:"template"`
+	Text       Text       `json:"text"`
+	Type       string     `json:"type"`
+	Visible    bool       `json:"visible"`
+	X          float64    `json:"x"`
+	Y          float64    `json:"y"`
+}
+
+type Point struct {
+	X float32 `json:"x"`
+	Y float32 `json:"y"`
+}
+
+type Text struct{}
